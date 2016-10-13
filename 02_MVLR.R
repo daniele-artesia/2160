fe3bf184e9b94f017cb0b67a198cb3449da29a11
daniele-artesia <daniele@artesia-consulting.co.uk>
2016-10-13 13:04:01 +0100

    create empty time series data frame

--- 02_MVLR.R
+++ 02_MVLR.R
@@ -40,8 +40,8 @@ meter.reads <- read.csv("T:\\live\\2160 SWW PCC Sept 2016/02 Delivery/R input fi
 meter.reads$pRef <- as.numeric(as.character(meter.reads$pRef))
 meter.reads <- meter.reads[order(meter.reads$pRef),]
 
-mr. <- as.character(unique(meter.reads$pRef)) 
-mr. <- mr.[order(mr.)]
+#mr. <- as.character(unique(meter.reads$pRef)) 
+#mr. <- mr.[order(mr.)]
 
 meter.reads$Date <- as.Date(meter.reads$Date, format="%d/%m/%Y")
 meter.reads$Reading <- as.numeric(as.character(meter.reads$Reading))
@@ -67,14 +67,14 @@ meter.reads <- meter.reads[meter.reads$excl==0,]
 meter.reads <- meter.reads[!is.na(meter.reads$excl),]
 meter.reads <- meter.reads[order(meter.reads$pRef),]
 
-#remove reading prior 2005 (questionnaire dates back to 2005 only) & meter read from 2015 as they are wrong according to previous model
-meter.reads.1 <- subset(meter.reads, meter.reads$Date >= "2005-01-01" & meter.reads$Date < "2015-01-01") 
-meter.reads.2 <- subset(meter.reads, meter.reads$Date >= "2016-01-01")
-meter.reads <- rbind(meter.reads.1,meter.reads.2)
-meter.reads <- meter.reads[order(meter.reads$Date),]
-
-rm(meter.reads.1)
-rm(meter.reads.2)
+# #remove reading prior 2005 (questionnaire dates back to 2005 only) & meter read from 2015 as they are wrong according to previous model
+# meter.reads.1 <- subset(meter.reads, meter.reads$Date >= "2005-01-01" & meter.reads$Date < "2015-01-01") 
+# meter.reads.2 <- subset(meter.reads, meter.reads$Date >= "2016-01-01")
+# meter.reads <- rbind(meter.reads.1,meter.reads.2)
+# meter.reads <- meter.reads[order(meter.reads$Date),]
+# 
+# rm(meter.reads.1)
+# rm(meter.reads.2)
 
 meter.reads$pRef <- as.numeric(as.character(meter.reads$pRef))
 meter.reads <- meter.reads[order(meter.reads$pRef),]
@@ -87,37 +87,46 @@ survey.properties$pRef <- as.numeric(as.character(survey.properties$pRef))
 
 total.s <- merge(survey.properties, meter.reads[meter.reads$watStatOnReadDate == "u",], by="pRef")
 
-# calculate difference per reading
+
 total.s <- total.s[order(total.s$pRef, total.s$Date),]
 t. <- as.numeric(as.character(unique(meter.reads$pRef)))
 
-############### for total property without discrimination by zone######
-for (j in t.[1])
-{
-  i <- total.s[total.s$pRef==j,]
-  i <- i[!is.na(i$pRef),]
-  i <- i[order(i$Date),]
-  i$date.dif <- diff(c(NA,i$Date))
-  i$reading.dif <- diff(c(NA,i$Reading))
-  assign("total.PHC",i)
-}
+############### # calculate difference per reading ###################################
+require(dplyr)
 
-for (j in t.[2:length(t.)])
-{
-  i <- total.s[total.s$pRef==j,]
-  i <- i[!is.na(i$pRef),]
-  i <- i[order(i$Date),]
-  i$date.dif <- diff(c(NA,i$Date))
-  i$reading.dif <- diff(c(NA,i$Reading))
-  total.PHC <- rbind(total.PHC,i)
-}
+total.PHC<- total.s %>% 
+  arrange(pRef, Date)%>%
+  group_by(pRef)%>%
+  mutate(date.dif=c(NA,diff(Date)))%>%
+  mutate(reading.dif=c(NA,diff(Reading)))
+
+######## Sarah's loop#####
+# for (j in t.[1])
+# {
+#   i <- total.s[total.s$pRef==j,]
+#   i <- i[!is.na(i$pRef),]
+#   i <- i[order(i$Date),]
+#   i$date.dif <- diff(c(NA,i$Date))
+#   i$reading.dif <- diff(c(NA,i$Reading))
+#   assign("total.PHC",i)
+# }
+# 
+# for (j in t.[2:length(t.)])
+# {
+#   i <- total.s[total.s$pRef==j,]
+#   i <- i[!is.na(i$pRef),]
+#   i <- i[order(i$Date),]
+#   i$date.dif <- diff(c(NA,i$Date))
+#   i$reading.dif <- diff(c(NA,i$Reading))
+#   total.PHC <- rbind(total.PHC,i)
+# }
 
 #Create new dif reading which replaces negative diffs (likely where meter has been replaced) with meter read (likely the value acrued since new meter installed)
 total.PHC$reading.dif2 <- ifelse(total.PHC$reading.dif<0,total.PHC$Reading,total.PHC$reading.dif)
 
 ## calculate PHC with method 2  fro each reading 
 total.PHC$PHC2 <- total.PHC$reading.dif2/total.PHC$date.dif 
-View(total.PHC[,c(1, 3,13, 23, 25)])
+View(total.PHC[,c(1, 3,13, 22, 23, 25)])
 
 
 ### check outliers#####################################################
@@ -130,8 +139,8 @@ ggplot(melt(total.PHC[,c(3,25)]), aes(surveyType, value)) + geom_boxplot()
 blind.PHC <- subset(total.PHC, total.PHC$surveyType == "Blind")
 sodwac.PHC <- subset(total.PHC, total.PHC$surveyType == "Sodwac")
 
-blind.outliers <- boxplot.stats(blind.PHC$PHC2)$out
-sodwac.outliers <- boxplot.stats(sodwac.PHC$PHC2)$out
+#blind.outliers <- boxplot.stats(blind.PHC$PHC2)$out
+#sodwac.outliers <- boxplot.stats(sodwac.PHC$PHC2)$out
 
 ### outliers detector  function ###### remove outliers with Tukey's method (outliers ranged above and below the 1.5*IQR)
 outlierKD <- function(dt, var) {
@@ -166,42 +175,57 @@ outlierKD <- function(dt, var) {
   }
 }
 #############################################################
-#remove outliers
+#remove outliers for each surveyType
 
-blind <- blind.PHC
+blind <- blind.PHC # backups
 sodwac <- sodwac.PHC
 
-outlierKD(blind, PHC2)
+outlierKD(blind, PHC2) # need to write yes to both - will put outliers to NA
 outlierKD(sodwac, PHC2)
 
 
-t.PHC <- rbind(blind, sodwac) 
+total.PHC <- rbind(blind, sodwac)
+total.PHC$pRef <- as.numeric(as.character(total.PHC$pRef))
+total.PHC <- t.PHC[order(total.PHC$pRef, total.PHC$Date),]
 
 # ########################### create time series ############################################################################################
+New<- as.data.frame(seq.Date(as.Date(min(total.PHC$Date)), as.Date(max(total.PHC$Date)), by="days"))
+colnames(New)[1] <- "Date"
+New.2 <-as.data.frame(matrix(nrow = 3183, ncol = 1737))
+pRef.list <- as.character(unique(total.PHC$pRef))
+colnames(New.2) <- pRef.list
+
+timeserie <- cbind(New, New.2)
+
+rm(New)
+rm(New.2)
+
+
+
 
 #sarah's method
-New <- as.data.frame(seq.Date(as.Date(min(t.PHC$Date)), as.Date(max(t.PHC$Date)), by="days"))
-colnames(New)[1] <- "Date"
-
-PHC.list <- as.character(unique(t.PHC$pRef))
-
-for (i in PHC.list)
-{
-  j <- i
-  i <- t.PHC[total.PHC$pRef==i,]
-  i <- i[,c("Date","PHC2")]
-  colnames(i)[2] <- j
-  New<-merge(New,i,by="Date", all.x=T)
-}
-
-New <- New[order(rev(New$Date)),]
-for (i in PHC.list)
-{
-  for (j in 1:nrow(New))
-  {
-
-    New[j,i] <- ifelse(is.na(New[j,i]),New[(j-1),i],New[j,i])
-  }
-}
-New <- New[order(New$Date),]
+# New <- as.data.frame(seq.Date(as.Date(min(t.PHC$Date)), as.Date(max(t.PHC$Date)), by="days"))
+# colnames(New)[1] <- "Date"
+# 
+# PHC.list <- as.character(unique(t.PHC$pRef))
+# 
+# for (i in PHC.list)
+# {
+#   j <- i
+#   i <- t.PHC[total.PHC$pRef==i,]
+#   i <- i[,c("Date","PHC2")]
+#   colnames(i)[2] <- j
+#   New<-merge(New,i,by="Date", all.x=T)
+# }
+# 
+# New <- New[order(rev(New$Date)),]
+# for (i in PHC.list)
+# {
+#   for (j in 1:nrow(New))
+#   {
+# 
+#     New[j,i] <- ifelse(is.na(New[j,i]),New[(j-1),i],New[j,i])
+#   }
+# }
+# New <- New[order(New$Date),]
 
